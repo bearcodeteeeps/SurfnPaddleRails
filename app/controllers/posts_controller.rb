064a2408e18e539c2b9ddf5d3c088ea14700ab2a
J-Henry <JH@JH.local>
2016-03-30 15:06:54 -0400

    Add posts/new form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,38 @@
 class PostsController < ApplicationController
 
 def index
-  @posts = Post.all.sort_by { |post| post.created_at }
+  @posts = Post.order('created_at DESC').all
   @post = @posts.last
+end
+
+def new
+  @post = Post.new
+end
+
+def create
+  @post = Post.new(post_params)
+  @post.save
+  redirect_to @post
 end
 
 def show
   @posts = Post.all.sort_by { |post| post.created_at }.reverse
   @post = Post.find(params[:id])
+end
+
+def edit
+end
+
+def update
+end
+
+def distroy
+end
+
+private
+
+def post_params
+  params.require(:post).permit(:author, :title, :body)
 end
 
 end
